111520f6010465cf830a25b96a2eea302c85729c
jagruthi masadi <jagruthimasadi98@gmail.com>
2023-10-03 15:52:49 -0500

    managed prometheus

---
+++ modules/monitor/azure-monitor/variables.tf
@@ -0,0 +1,23 @@
+variable "client_secret" {
+  description = "Azure service principal client secret"
+}
+
+variable "client_id" {
+  description = "Azure service principal client ID"
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+}
+
+variable "resource_group_name" {
+  description = "Name of the existing Azure resource group"
+}
+
+variable "vnet_name" {
+  description = "Name of the existing Virtual Network"
+}
